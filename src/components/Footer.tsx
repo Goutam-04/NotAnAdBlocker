09f7ae8245d77216750fdd86a499346f08024370
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-06-12 00:28:16 +0530

    completed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,14 +6,7 @@ export function Footer() {
     <div className="relative">
       <Container>
         <div className="text-sm text-center text-gray-600 dark:text-gray-400 py-6">
-          Copyright © {new Date().getFullYear()}. Made with ♥ by{" "}
-          <a href="https://web3templates.com/" target="_blank" rel="noopener">
-            Web3Templates.
-          </a>{" "}
-          Illustrations from{" "}
-          <a href="https://www.glazestock.com/" target="_blank" rel="noopener">
-            Glazestock
-          </a>
+          Copyright © {new Date().getFullYear()}. Made with ♥ by Goutam.
         </div>
       </Container>
     </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,24 +8,24 @@ export const Hero = () => {
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-              Free Landing Page Template for startups
+            Ad-Free YouTube Experience with an Extension
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-              Nextly is a free landing page & marketing website
-              template for startups and indie projects. Its built with
-              Next.js & TailwindCSS. And its completely open-source.
+            It automatically skips ads and speeds them up.<br></br>
+            It's user-friendly, efficient, and completely free.<br></br>
+            Watch the video or go to github repo for installation process.
             </p>
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
               <a
-                href="https://web3templates.com/templates/nextly-landing-page-template-for-startups"
+                href="/NotAnAdBlocker.zip"
                 target="_blank"
                 rel="noopener"
                 className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
                 Download for Free
               </a>
               <a
-                href="https://github.com/web3templates/nextly-template/"
+                href="https://github.com/Goutam-04/NotAnAdBlocker"
                 target="_blank"
                 rel="noopener"
                 className="flex items-center space-x-2 text-gray-500 dark:text-gray-400">
@@ -46,7 +46,7 @@ export const Hero = () => {
           </div>
         </div>
         <div className="flex items-center justify-center w-full lg:w-1/2">
-        <Video videoId="fZ0D0cnR88E" />
+        <Video videoId="gPLrIJgsJlw" />
         </div>
       </Container>
     </>
